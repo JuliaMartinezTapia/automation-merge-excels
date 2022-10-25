89f52ca9cc84436b41959fc159bdb7b5bb6febf6
Julia <78017909+JuliaMartinezTapia@users.noreply.github.com>
2022-10-25 19:17:59 +0200

    commit inicial

---
+++ merge_excels.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from functools import reduce
+import pandas as pd
+
+#List to store the dataframes extacted from the excel files located in current directory (same directory than this .py file)
+
+list_df = []
+
+#Loop to go through the elements (directories and files) within the current directory,find the xlsx files, 
+#appply a mask to eliminate spaces and Nans in the first column (on which the merge will be subsequently performed).
+
+for item in os.listdir(Path.cwd()):
+    
+    if "xlsx" in item:
+
+        df = pd.read_excel(item)
+        name_col = df.columns[0]
+        df = df[((df[name_col]) != " ")][~df[name_col].isna()]
+        list_df.append(df)
+
+list_df  
+
+#Merging the dataframes in the list using funcional programming (reduce).
+
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=[name_col],
+                                            how='outer'), list_df)
+
+#Download the merged dataframe to an excel spreadsheet created in the current directory
+
+df_merged.to_excel("merged_df.xlsx")
